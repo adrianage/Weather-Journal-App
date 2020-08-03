53106dd9290ff93c85fa97d950a2e2032035c61a
adrianage <64158136+adrianage@users.noreply.github.com>
2020-08-03 06:48:48 -0700

    Tidied up JS and app name on package.json

--- website/app.js
+++ website/app.js
@@ -1,39 +1,38 @@
 /* Global Variables */
-const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const baseLink = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 const apiKey = '6d535f8ac21002b728c1ab86702dacf1';
 
-const submitBtn = document.getElementById('generate');
+const button = document.getElementById('generate');
 
-submitBtn.addEventListener('click', async () => {
+button.addEventListener('click', async () => {
+    button.textContent = 'Generating...';
+});
 
-    //Getting user responses from the website
-    submitBtn.textContent = 'Generating...';
+/* Function to GET Web API Data*/
+async function openWeatherData(apiKey) {
+    let zipCode = document.getElementById("zip").value;
+    fetch(baseLink + zipCode + ",us&appid=" + apiKey).then((response) => {
+        return response.json();
+    }).then((result) => {
+        postProjectData(result.main.temp);
+    });
+}
+/* Function to GET Project Data */
+async function getProjectData() {
+    fetch(baseLink + 'projectData').then((response) => {
+        return response.json();
+    }).then((result) => {
+        document.getElementById("date").innerHTML = result.date;
+        document.getElementById("temp").innerHTML = JSON.stringify(result.temp);
+        document.getElementById("content").innerHTML = result.content;
+        document.getElementById("zip").value = "";
+        document.getElementById("feelings").value = "";
+    });
+}
 
-    /* Function to GET Web API Data*/
-    async function getDataFromOpenWeatherAPI(apiKey) {
-        let zipCode = document.getElementById("zip").value;
-        fetch(baseUrl + zipCode + ",us&appid=" + apiKey).then((response) => {
-            return response.json();
-        }).then((result) => {
-            postProjectData(result.main.temp);
-        });
-    }
-    /* Function to GET Project Data */
-    async function getProjectData() {
-        fetch(baseUrl + 'projectData').then((response) => {
-            return response.json();
-        }).then((result) => {
-            document.getElementById("date").innerHTML = result.date;
-            document.getElementById("temp").innerHTML = JSON.stringify(result.temp);
-            document.getElementById("content").innerHTML = result.content;
-            document.getElementById("zip").value = "";
-            document.getElementById("feelings").value = "";
-        });
-    }
-})
 /* Function to POST Project data */
 async function postProjectData(temp) {
-    // Create a new date instance dynamically with JS
+    // Create a new date
     let d = new Date();
     let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
     let content = document.getElementById("feelings").value;
@@ -50,7 +49,7 @@ async function postProjectData(temp) {
         },
         body: JSON.stringify(data),
     };
-    fetch(baseURL + 'projectData', options).then((response) => {
+    fetch(openWeatherData + 'projectData', options).then((response) => {
         return response.json();
     }).then((result) => {
         getProjectData()
