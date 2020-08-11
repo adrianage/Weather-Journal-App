eae62336ce9e6515f6f1dcaa222c17ebb7e77e41
adrianage <64158136+adrianage@users.noreply.github.com>
2020-08-11 01:41:33 -0700

    updated js

--- website/app.js
+++ website/app.js
@@ -4,37 +4,37 @@ const apiKey = '6d535f8ac21002b728c1ab86702dacf1';
 
 const button = document.getElementById('generate');
 
+// const openWeatherData =
+/* Function to GET Web API Data*/
+async function openWeatherData(apiKey) {
+    let zipCode = document.getElementById("zip").value;
+    fetch(baseLink + zipCode + ",us&appid=" + apiKey).then((response) => {
+        return response.json();
+    }).then((result) => {
+        postProjectData(result.main.temp);
+    });
+}
+
+/* Function to GET Project Data */
+// const getProjectData =
+async function getProjectData() {
+    fetch(baseLink + 'projectData').then((response) => {
+        return response.json();
+    }).then((result) => {
+        document.getElementById("date").innerHTML = result.date;
+        document.getElementById("temp").innerHTML = JSON.stringify(result.temp);
+        document.getElementById("content").innerHTML = result.content;
+        document.getElementById("city").innerHTML = result.city;
+        document.getElementById("zip").value = "";
+        document.getElementById("feelings").value = "";
+    });
+}
+
 button.addEventListener('click', async () => {
     button.textContent = 'Generating...';
+    openWeatherData();
+    getProjectData();
 });
-
-const openWeatherData =
-    /* Function to GET Web API Data*/
-    async function openWeatherData(apiKey) {
-        let zipCode = document.getElementById("zip").value;
-        fetch(baseLink + zipCode + ",us&appid=" + apiKey).then((response) => {
-            return response.json();
-        }).then((result) => {
-            postProjectData(result.main.temp);
-        });
-    }
-openWeatherData();
-/* Function to GET Project Data */
-const getProjectData =
-    async function getProjectData() {
-        fetch(baseLink + 'projectData').then((response) => {
-            return response.json();
-        }).then((result) => {
-            document.getElementById("date").innerHTML = result.date;
-            document.getElementById("temp").innerHTML = JSON.stringify(result.temp);
-            document.getElementById("content").innerHTML = result.content;
-            document.getElementById("city").innerHTML = result.city;
-            document.getElementById("zip").value = "";
-            document.getElementById("feelings").value = "";
-        });
-    }
-
-getProjectData();
 
 /* Function to POST Project data */
 async function postProjectData(temp) {
